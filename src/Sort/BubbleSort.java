2d12c18bdf3c5aa55183fc13fdf473c8abc60056
syed <syed@syed-HP-Laptop-15-dw2xxx>
2022-04-04 11:59:58 +0530

    This is the Second commit of Sort.

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -1,11 +1,15 @@
 package Sort;
 
 public class BubbleSort {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int arr[] = {9, 6, 8, 7, 3, 5, 4, 1, 2, 0};
-		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 9; j++) {
-				if(arr[j] > arr[j+1]) {
+		for(int i = 0; i < 10; i++) 
+		{
+			for(int j = 0; j < 9; j++) 
+			{
+				if(arr[j] > arr[j+1]) 
+				{
 					int temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
